41be3befb77adbcc6fb93de8686f48b4d256937a
Elliot Revan <elliotrevan@gmail.com>
2019-11-26 14:47:57 +0000

    public accessible costs from outside styled components

--- src/theme.ts
+++ src/theme.ts
@@ -1,38 +1,44 @@
-import { createGlobalStyle, DefaultTheme } from 'styled-components';
+import { DefaultTheme } from 'styled-components';
 
-createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css?family=Quicksand:400,500,700&display=swap');
-    font-family: 'Quicksand', sans-serif;
-    font-size: 16px;
-`;
+export const fontSizes: { [key: string]: string } = {
+    extraTiny: '0.6.25rem',
+    tiny: '0.75rem',
+    small: '0.875rem',
+    medium: '1rem',
+    large: '1.125rem',
+    extraLarge: '1.25rem',
+    extraExtraLarge: '1.5rem',
+    extraExtraExtraLarge: '2rem',
+};
+
+export const breakpoints = {
+    large: '1400px',
+};
+
+export const globalPadding = '10vw';
+
+export const spacing = {
+    tiny: '8px',
+    small: '16px',
+    medium: '24px',
+    large: '48px',
+    extraLarge: '64px',
+};
+
+export const colors: { [key: string]: string } = {
+    white: '#F3F7F0',
+    darkGrey: '#343434',
+    blue: '#1B4965',
+    pink: '#A93F55',
+    yellow: '#FFC857',
+};
 
 const theme: DefaultTheme = {
-    fontSizes: {
-        extraTiny: '0.6.25rem',
-        tiny: '0.75rem',
-        small: '0.875rem',
-        medium: '1rem',
-        large: '1.125rem',
-        extraLarge: '1.25rem',
-        extraExtraLarge: '1.5rem',
-        extraExtraExtraLarge: '2rem',
-    },
-
-    spacing: {
-        tiny: '8px',
-        small: '16px',
-        medium: '24px',
-        large: '48px',
-        extraLarge: '64px',
-    },
-
-    colors: {
-        white: '#F3F7F0',
-        darkGrey: '#343434',
-        blue: '#1B4965',
-        pink: '#A93F55',
-        yellow: '#FFC857',
-    },
+    fontSizes,
+    breakpoints,
+    globalPadding,
+    spacing,
+    colors,
 };
 
 export default theme;
