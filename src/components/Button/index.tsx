36b390ecff5347e8cd987974fced14d8aaf7dc28
Elliot Revan <elliotrevan@gmail.com>
2019-11-30 12:18:27 +0000

    tests for button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,104 +1,20 @@
 import React, { FunctionComponent } from 'react';
-import styled from 'styled-components';
-import { ColorType, fontSizes } from 'src/theme';
+import { ColorType } from 'src/theme';
 
-const ButtonStyled = styled.button`
-    font-size: ${({
-        theme: {
-            fontSizes: { medium },
-        },
-    }) => medium};
-    padding: ${({
-        theme: {
-            spacing: { tiny, medium },
-        },
-    }) => `${tiny} ${medium}`};
-    font-weight: 500;
-    text-transform: uppercase;
-    transition: color 0.3s ease, background-color 0.3s ease;
-    background-color: ${({
-        color,
-        theme: {
-            colors,
-            colors: { blue },
-        },
-    }) => (color ? colors[`${color}`] : blue)};
-    border-width: 0.12rem;
-    border-style: solid;
-    border-radius: 1.5rem;
-    border-color: ${({
-        color,
-        theme: {
-            colors,
-            colors: { blue },
-        },
-    }) => (color ? colors[`${color}`] : blue)};
-    color: ${({
-        color,
-        theme: {
-            colors: { white, darkGrey },
-        },
-    }) => {
-        switch (color) {
-            case 'white':
-                return darkGrey;
-            case 'yellow':
-                return darkGrey;
-            case 'blue':
-                return white;
-            case 'pink':
-                return white;
-            case 'darkGrey':
-                return white;
-            default:
-                return white;
-        }
-    }};
+import { ButtonStyled } from './styles';
 
-    &:hover,
-    &:focus {
-        background-color: ${({
-            color,
-            theme: {
-                colors: { white },
-            },
-        }) => {
-            switch (color) {
-                case 'white':
-                    return `rgba(0, 0, 0, 0.4)`;
-                case 'yellow':
-                    return `rgba(0, 0, 0, 0.4)`;
-                case 'blue':
-                    return white;
-                case 'pink':
-                    return white;
-                case 'darkGrey':
-                    return white;
-                default:
-                    return white;
-            }
-        }};
-        color: ${({
-            color,
-            theme: {
-                colors,
-                colors: { blue },
-            },
-        }) => (color ? colors[`${color}`] : blue)};
-    }
-`;
 type Props = {
     color?: ColorType;
     onClick?: () => void;
 };
 
 export const Button: FunctionComponent<Props> = ({
-    color,
-    onClick,
+    color = 'blue',
+    onClick = () => null,
     children,
 }) => {
     return (
-        <ButtonStyled onClick={onClick} color={color || 'blue'}>
+        <ButtonStyled onClick={onClick} color={color}>
             {children}
         </ButtonStyled>
     );
