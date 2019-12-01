271e18c6871d55071171f4df357baa424bfc887d
Elliot Revan <elliotrevan@gmail.com>
2019-12-01 17:55:45 +0000

    cleaning user card styles

--- src/components/UserCard/index.tsx
+++ src/components/UserCard/index.tsx
@@ -1,51 +1,15 @@
 import React from 'react';
-import styled from 'styled-components/macro';
 
 import { H3, Span, Paragraph } from 'src/components/Typography';
 import { VerticalSpacer, RowCenter, ColumnCenter } from 'src/components/Layout';
 
-const UserCardWrapper = styled.div`
-    border-radius: 10px;
-    overflow: hidden;
-`;
-
-const CoverColor = styled.div`
-    background-color: ${({ color }) => color};
-    width: 100%;
-    height: 150px;
-    position: relative;
-`;
-
-const BottomHalf = styled.div`
-    background-color: ${({
-        theme: {
-            colors: { white },
-        },
-    }) => white};
-    padding: ${({
-        theme: {
-            spacing: { medium, large },
-        },
-    }) => `${large} ${medium} ${medium} ${medium}`};
-`;
-
-const ImageWrapper = styled.div`
-    height: 128px;
-    width: 128px;
-    border-radius: 6px;
-    overflow: hidden;
-    border: ${({
-        theme: {
-            colors: { lightGrey },
-        },
-    }) => `solid 1px ${lightGrey}`};
-    position: absolute;
-    bottom: -40px;
-`;
-const ImageContainer = styled.img`
-    height: 128px;
-    width: 128px;
-`;
+import {
+    UserCardWrapper,
+    CoverColor,
+    ImageWrapper,
+    ImageContainer,
+    BottomHalf,
+} from './styles';
 
 export type UserType = {
     name: { first: string; last: string };
@@ -70,7 +34,6 @@ type Props = {
 };
 
 export const UserCard = ({
-    user,
     user: {
         name: { first, last },
         picture,
@@ -92,33 +55,28 @@ export const UserCard = ({
             </RowCenter>
             <BottomHalf>
                 <VerticalSpacer size='small' />
-                <RowCenter>
-                    <ColumnCenter>
-                        <H3>
-                            {first} {last}
-                        </H3>
-                        <VerticalSpacer size='small' />
-
-                        <Paragraph>
-                            <Span bold={true}>Email: </Span> {email}
-                        </Paragraph>
-                        <Paragraph>
-                            <Span bold={true}>Street: </Span>{' '}
-                            <Span>
-                                {street.number} {street.name}
-                            </Span>
-                        </Paragraph>
-                        <Paragraph>
-                            <Span bold={true}>City: </Span> {city}
-                        </Paragraph>
-                        <Paragraph>
-                            <Span bold={true}>State: </Span> {state}
-                        </Paragraph>
-                        <Paragraph>
-                            <Span bold={true}>Postcode: </Span> {postcode}
-                        </Paragraph>
-                    </ColumnCenter>
-                </RowCenter>
+                <H3>
+                    {first} {last}
+                </H3>
+                <VerticalSpacer size='small' />
+                <Paragraph>
+                    <Span bold={true}>Email: </Span> {email}
+                </Paragraph>
+                <Paragraph>
+                    <Span bold={true}>Street: </Span>{' '}
+                    <Span>
+                        {street.number} {street.name}
+                    </Span>
+                </Paragraph>
+                <Paragraph>
+                    <Span bold={true}>City: </Span> {city}
+                </Paragraph>
+                <Paragraph>
+                    <Span bold={true}>State: </Span> {state}
+                </Paragraph>
+                <Paragraph>
+                    <Span bold={true}>Postcode: </Span> {postcode}
+                </Paragraph>
             </BottomHalf>
         </UserCardWrapper>
     );
---
+++ src/components/UserCard/styles.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components/macro';
+
+export const UserCardWrapper = styled.div`
+    overflow: hidden;
+`;
+
+export const CoverColor = styled.div`
+    background-color: ${({ color }) => color};
+    width: 100%;
+    height: 150px;
+    position: relative;
+`;
+
+export const BottomHalf = styled.div`
+    background-color: ${({
+        theme: {
+            colors: { white },
+        },
+    }) => white};
+    padding: ${({
+        theme: {
+            spacing: { medium, large },
+        },
+    }) => `${large} ${medium} ${medium} ${medium}`};
+`;
+
+export const ImageWrapper = styled.div`
+    height: 128px;
+    width: 128px;
+    border-radius: 6px;
+    overflow: hidden;
+    border: ${({
+        theme: {
+            colors: { lightGrey },
+        },
+    }) => `solid 1px ${lightGrey}`};
+    position: absolute;
+    bottom: -40px;
+`;
+
+export const ImageContainer = styled.img`
+    height: 128px;
+    width: 128px;
+`;
