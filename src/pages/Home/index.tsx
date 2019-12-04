a8811866de6652843d855fcab247a592e17f27af
Elliot Revan <elliotrevan@gmail.com>
2019-12-04 15:54:49 +0000

    fix spelling

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { H1, H2, Paragraph } from 'src/components/Typography';
 import { PageWrapper, PageContent } from 'src/components/Layout';
-import AnitmatedContentWrapper from 'src/components/AnitmatedContentWrapper';
+import AnimatedContentWrapper from 'src/components/AnimatedContentWrapper';
 import {
     ContentWrapper,
     VerticalSpacer,
@@ -21,11 +21,11 @@ export const Home = () => {
                         <VerticalSpacer size='large' />
                         <Paragraph>
                             Welcome to my front-end showcase. I originally
-                            created this site to try out TypeScript instead of
-                            using Flow. This was also an exersice to see if I
-                            could make a website using pure functional
-                            components and hooks without using classes and their
-                            lifecycle functions.
+                            created this site to try out TypeScript as an
+                            alternative to JS + Flow. This was also an exercise
+                            to see if I could make a website using pure
+                            functional components and hooks without touching
+                            classes and their life cycle functions.
                         </Paragraph>
                     </ContentWrapper>
                 </ColorBlock>
@@ -35,7 +35,7 @@ export const Home = () => {
                 </ColorBlock>
 
                 <ColorBlock color='darkGrey'>
-                    <AnitmatedContentWrapper>
+                    <AnimatedContentWrapper>
                         <H2 color='white'> under the hood</H2>
 
                         <VerticalSpacer size='medium' />
@@ -52,7 +52,7 @@ export const Home = () => {
                             <br />
                             Jest + Enzyme snapshot regression testing
                         </Paragraph>
-                    </AnitmatedContentWrapper>
+                    </AnimatedContentWrapper>
                 </ColorBlock>
             </PageContent>
         </PageWrapper>
